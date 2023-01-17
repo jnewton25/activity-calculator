6ecdb1cb90262b8db8338bada8508e3fd5ed444c
Justin Newton <justinmnewton25@gmail.com>
2023-01-17 14:50:35 -0600

    Background and centered calculator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import "./index.css";
+import Wrapper from "./components/Wrapper";
 
 /* Figma wireframe -> 
 https://www.figma.com/file/1nxXwpQWoGcgcEtuZ03z9X/calculator?node-id=0%3A1&t=xMu8lHwpXVPGuvdF-0 
 */
 
-function App() {
-	function updateTotal(e) {}
-
-	return (
-		<div className="App">
-			<p>Hello</p>
-			<button onClick={updateTotal}>1</button>
-		</div>
-	);
-}
+const App = () => {
+	return <Wrapper></Wrapper>;
+};
 
 export default App;
